63e7422b6a5b89bb5e3d80dfc1430442d6dd5ea7
Laszlo Magyar <lmagyar1973@gmail.com>
2026-01-14 12:59:40 +0100

    fix service dependencies and dependency loops

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -107,12 +107,17 @@ fi
 if bashio::config.true "userspace_networking" || \
     bashio::config.false "accept_dns";
 then
+    # either this or init-magicdns-proxies/dependencies.d/tailscaled below has to be removed
     rm /etc/s6-overlay/s6-rc.d/tailscaled/dependencies.d/magicdns-egress-proxy
 fi
 # Disable MagicDNS ingress proxy service when userspace-networking is enabled
 if bashio::config.true "userspace_networking"; then
     rm /etc/s6-overlay/s6-rc.d/forwarding/dependencies.d/magicdns-ingress-proxy
     rm /etc/s6-overlay/s6-rc.d/user/contents.d/magicdns-ingress-proxy
+    if bashio::config.true "accept_dns"; then
+        # either this or tailscaled/dependencies.d/magicdns-egress-proxy above has to be removed
+        rm /etc/s6-overlay/s6-rc.d/init-magicdns-proxies/dependencies.d/tailscaled
+    fi
 fi
 
 # Disable protect-subnets service when userspace-networking is enabled or accepting routes is disabled
